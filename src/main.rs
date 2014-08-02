3de7e4a978adbbba881e7cb7c255cf6e78734d2b
Steve Klabnik <steve@steveklabnik.com>
2014-08-02 00:35:29 -0400

    Much better.

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,26 @@ struct Philosopher {
     right_hand: int,
 }
 
+impl Philosopher {
+    fn eat(&self) {
+        println!("{} is eating.", self.name);
+    }
+
+    fn new(name: &str,
+           left_hand: int,
+           right_hand: int) -> (Philosopher, Receiver<int>) {
+        let (tx, rx) = channel();
+        let p = Philosopher {
+            name: name.to_string(),
+            channel: tx,
+            left_hand: 1,
+            right_hand: 2,
+        };
+        
+        (p, rx)
+    }
+}
+
 enum Status {
     OnTable,
     InUse,
@@ -15,39 +35,36 @@ struct Chopstick(Status);
 fn main() {
     let channels = Vec::from_fn(4, |_| channel::<int>());
 
-    let philosophers = vec![
-        Philosopher {
-            name: "Karl Marx".to_string(),
-            channel: channels[0].ref0().clone(),
-            left_hand: 1,
-            right_hand: 2,
-        },
-        Philosopher {
-            name: "Gilles Deleuze".to_string(),
-            channel: channels[1].ref0().clone(),
-            left_hand: 2,
-            right_hand: 3,
-        },
-        Philosopher {
-            name: "Baruch Spinoza".to_string(),
-            channel: channels[0].ref0().clone(),
-            left_hand: 3,
-            right_hand: 4,
-        },
-        Philosopher {
-            name: "Friedrich Nietzsche".to_string(),
-            channel: channels[0].ref0().clone(),
-            left_hand: 4,
-            right_hand: 5,
-        },
-        Philosopher {
-            name: "Michel Foucault".to_string(),
-            channel: channels[0].ref0().clone(),
-            // Foucault is left handed. ;)
-            left_hand: 1,
-            right_hand: 5,
-        },
-    ];
+    let (p, rx) = Philosopher::new("Karl Marx", 1, 2,);
+
+    spawn(proc() {
+        p.eat();
+    });
+
+    let (p, rx) = Philosopher::new("Gilles Deleuze", 2, 3);
+
+    spawn(proc() {
+        p.eat();
+    });
+
+    let (p, rx) = Philosopher::new("Baruch Spinoza", 3, 4);
+
+    spawn(proc() {
+        p.eat();
+    });
+
+    let (p, rx) = Philosopher::new("Friedrich Nietzsche", 4, 5);
+
+    spawn(proc() {
+        p.eat();
+    });
+
+    // Foucault is left handed. ;)
+    let (p, rx) = Philosopher::new("Michel Foucault", 1, 5);
+
+    spawn(proc() {
+        p.eat();
+    });
 
     let mut chopsticks = Vec::from_fn(5, |_| Chopstick(OnTable));
 }
